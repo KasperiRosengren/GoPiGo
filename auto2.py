d00803be5f1691672f03cd946e3ec79aa68c800a
KasperiRosengren <64841829+KasperiRosengren@users.noreply.github.com>
2021-11-23 13:42:28 +0200

    update from car

--- auto2.py
+++ auto2.py
@@ -8,7 +8,7 @@ lf = EasyLineFollower()
 dist_sens = gpg.init_distance_sensor()
 serv=gpg.init_servo()
 
-gpg.set_speed(200)
+#gpg.set_speed(250)
 
 setpoint = 0.5
 integralArea = 0.0
@@ -21,11 +21,11 @@ Ki = 0.0 # ditto
 Kd = 2500.0 # ditto as above
 
 NeedToNavigate = True
-myLocationX = 0
-myLocationY = 0
+myLocationX = 4
+myLocationY = 8
 destinationX = 0
 destinationY = 3
-myHeading = 90
+myHeading = 270
 NeedToChangeX = False
 NeedToChangeY = False
 newGrid = True
@@ -34,7 +34,7 @@ ServoPosRight = 160
 ServoPosLeft = 40
 ServoPosCenter = 100
 
-MinDistance = 10
+MinDistance = 150
 AllWhiteCounter = 0
 
 def AlterHeading(degrees):
@@ -94,52 +94,45 @@ def DecideTurn(direction):
     time.sleep(0.3)
 
 
-def GOForward():
+
+def GOForward(values):
     global AllWhiteCounter
     if(IsItSafeToGoForward()==True):
-        values = lf.position_01()
-        if(values[3] + values[4]==0 or values[0] + values[1] ==0):
-            gpg.stop()
-            gpg.drive_cm(5)
-            return 2
-        else:
-            if(values[1]==0 or values[2]==0 or values[3]==0):
-                if(values[1]==0 and values[2]==1):
-                    gpg.steer(30, 100)
-                elif(values[3]==0 and values[2]==1):
-                    gpg.steer(100, 30)
-                else:
-                    gpg.forward()
-                AllWhiteCounter=0
-                return 1
-                #print("Driving forward")
-            elif(values[4]==0 and values[3]==1):
-                gpg.right()
-                AllWhiteCounter=0
-                return 1
-                
-                #print("Driving right")
-            elif(values[0]==0 and values[1]==1):
-                gpg.left()
-                AllWhiteCounter=0
-                return 1
-                
-            elif(values[0]==1 and values[1]==1 and values[2]==1 and values[3]==1 and values[4]==1):
-                AllWhiteCounter += 1
-                if(AllWhiteCounter <= 40):
-                    gpg.steer(100, 40)
-                    return 1
-                else:
-                    print("Hjalp, Im lost :( ")
-                    while(True):
-                        gpg.stop()
+        #values = lf.position_01()
+        #print(str(values[0])+" "+str(values[1])+" "+str(values[2])+" "+str(values[3])+" "+str(values[4]))
+        #print(" "+str(values[0])+" "+str(values[1])+" "+str(values[2])+" "+str(values[3])+" "+str(values[4]))
+        if(values[1]==0 or values[2]==0 or values[3]==0):
+            if(values[1]==0 and values[2]==1):
+                gpg.steer(30, 100)
+            elif(values[3]==0 and values[2]==1):
+                gpg.steer(100, 30)
             else:
-                AllWhiteCounter=0
+                gpg.forward()
+            AllWhiteCounter=0
+            #print("Driving forward")
+        elif(values[4]==0 and values[3]==1):
+            gpg.right()
+            AllWhiteCounter=0
+            
+            #print("Driving right")
+        elif(values[0]==0 and values[1]==1):
+            gpg.left()
+            AllWhiteCounter=0
+            
+        elif(values[0]==1 and values[1]==1 and values[2]==1 and values[3]==1 and values[4]==1):
+            AllWhiteCounter += 1
+            if(AllWhiteCounter <= 40):
+                gpg.steer(100, 40)
+            else:
+                print("Hjalp, Im lost :( ")
+                while(True):
+                    gpg.stop()
+        else:
+            AllWhiteCounter=0
     else:
         gpg.stop()
-        return 0
-
-    time.sleep(0.02)
+        
+    #time.sleep(0.02)
             
             
 def TurnRight():
@@ -154,17 +147,21 @@ def Turn180():
     gpg.turn_degrees(193)
     AlterHeading(180)
 
+
 def IsItSafeToGoForward():
     global MinDistance
-    global ServoPosCenter
-    serv.rotate_servo(ServoPosCenter)
-    dist = dist_sens.read_mm()
-    if(dist <= MinDistance):
-        return False
-    else:
-        return True
+    #global ServoPosCenter
+    #serv.rotate_servo(ServoPosCenter)
+    return True
+    
+    #dist = dist_sens.read_mm()
+    #if(dist <= MinDistance):
+        #return False
+    #else:
+        #return True
     
 
+            
 def IsItSafeToCross():
     global ServoPosRight
     global ServoPosLeft
@@ -200,8 +197,12 @@ def IsItSafeToCross():
     serv.rotate_servo(ServoPosCenter)
     return True
 
-
 def NeedToChangeX():
+    global myLocationX
+    global myLocationY
+    global destinationX
+    global destinationY
+    global myHeading
     needToChangeX = True
     while needToChangeX:
             if(IsItSafeToCross()==False):
@@ -213,7 +214,16 @@ def NeedToChangeX():
                 if(myHeading == 90):
                     needToDrive = True
                     while(needToDrive):
-                        if(GOForward()==2):
+                        linevalues = lf.position_01()
+                        print(str(linevalues[0])+" "+str(linevalues[1])+" "+str(linevalues[2])+" "+str(linevalues[3])+" "+str(linevalues[4]))
+                        #GOForward(linevalues)
+                        
+                        time.sleep(0.01)
+                        if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
+                        #if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0):
+                            gpg.stop()
+                            gpg.drive_cm(5)
+                            
                             print("Grid forward on X")
                             myLocationX = myLocationX + 1
                             print("new location: "+str(myLocationX)+"."+str(myLocationY))
@@ -232,6 +242,7 @@ def NeedToChangeX():
                                         time.sleep(1)
                         else:
                             GOForward(linevalues)
+                            
                 else:
                     DecideTurn("right")
                     
@@ -244,8 +255,8 @@ def NeedToChangeX():
                         linevalues = lf.position_01()
                         print(str(linevalues[0])+" "+str(linevalues[1])+" "+str(linevalues[2])+" "+str(linevalues[3])+" "+str(linevalues[4]))
                         #GOForward(linevalues)
-                        #if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
-                        if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0):
+                        if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
+                        #if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0):
                             gpg.stop()
                             gpg.drive_cm(5)
                             print("Grid backward on X")
@@ -266,6 +277,7 @@ def NeedToChangeX():
                                         time.sleep(1)
                         else:
                             GOForward(linevalues)
+                
                 else:
                     DecideTurn("left")
 
@@ -273,6 +285,11 @@ def NeedToChangeX():
 
 
 def NeedToChangeY():
+    global myLocationX
+    global myLocationY
+    global destinationX
+    global destinationY
+    global myHeading
     needToChangeY = True
     while needToChangeY:
         if(IsItSafeToCross()==False):
@@ -287,8 +304,8 @@ def NeedToChangeY():
                     linevalues = lf.position_01()
                     print(str(linevalues[0])+" "+str(linevalues[1])+" "+str(linevalues[2])+" "+str(linevalues[3])+" "+str(linevalues[4]))
                     #GOForward(linevalues)
-                    #if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
-                    if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0 or linevalues[1] + linevalues[2] + linevalues[3] ==0 ):
+                    if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
+                    #if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0 or linevalues[1] + linevalues[2] + linevalues[3] ==0 ):
                         gpg.stop()
                         gpg.drive_cm(5)
                         print("Grid forward on Y")
@@ -309,6 +326,7 @@ def NeedToChangeY():
                                     time.sleep(1)
                     else:
                         GOForward(linevalues)
+                        
             else:
                 DecideTurn("up")
                 
@@ -323,8 +341,8 @@ def NeedToChangeY():
                     print(str(linevalues[0])+" "+str(linevalues[1])+" "+str(linevalues[2])+" "+str(linevalues[3])+" "+str(linevalues[4]))
                     #GOForward(linevalues)
 
-                    #if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
-                    if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0):
+                    if(linevalues[3] + linevalues[4] + linevalues[2] ==0 or linevalues[0] + linevalues[1] + linevalues[2] ==0):
+                    #if(linevalues[3] + linevalues[4]==0 or linevalues[0] + linevalues[1] ==0):
                         #newGrid = False
                         gpg.stop()
                         gpg.drive_cm(5)
@@ -352,11 +370,6 @@ def NeedToChangeY():
 
 
 def NavigateToLocation():
-    global myLocationX
-    global myLocationY
-    global destinationX
-    global destinationY
-    global myHeading
     NeedToNavigate=True
     while NeedToNavigate:
         if(destinationX != myLocationX):
@@ -374,7 +387,7 @@ def Main():
     
     while True:
         x = random.randint(0, 4)
-        y = random.randint(0, 7)
+        y = random.randint(0, 8)
         destinationX = x
         destinationY = y
         print("Destination: "+str(x)+"."+str(y))
@@ -384,5 +397,3 @@ def Main():
         
 
 Main()
-    
-    
