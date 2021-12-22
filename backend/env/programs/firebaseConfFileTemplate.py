34dd0b931e725604121c557072ab05662199e1f8
KasperiRosengren <64841829+KasperiRosengren@users.noreply.github.com>
2021-12-22 12:48:38 +0200

    Updated conf file template to include json creds

--- backend/env/programs/firebaseConfFileTemplate.py
+++ backend/env/programs/firebaseConfFileTemplate.py
@@ -9,3 +9,5 @@ config = {
   'measurementId': "G-W7SCB0XFZ8"
 }
 #All of these can be found under your firebase app project settings.
+confJSON = {'Link to your credentials json file'}
+#you need to create new under project settings in your firebase console.
